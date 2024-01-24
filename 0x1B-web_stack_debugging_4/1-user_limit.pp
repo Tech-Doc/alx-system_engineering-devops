481a98c8806343efba7c4934ea78ee570ebf420e
Tech-Doc <dr.stanleyperpetual@gmail.com>
2024-01-24 22:44:22 +0100

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,4 @@
 # This manuscript enables the user holberton to login and open files without error
-
 # Increase hard file limit for user holberton
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
